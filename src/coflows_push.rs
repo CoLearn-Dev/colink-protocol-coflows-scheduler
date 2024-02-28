da8aca776ec1dfd68430fee0315238c7a08c8ae4
stneng <git@stneng.com>
2024-02-28 04:57:05 +0000

    dispatch_point

--- src/coflows_push.rs
+++ src/coflows_push.rs
@@ -39,39 +39,40 @@ impl ProtocolEntry for Receiver {
             cl.create_entry(&message_id, &msg).await?;
             message_id
         };
-        let mut dispatch_point = "coflows_dispatch".to_string();
-        if let Ok(res) = cl
-            .read_entry(&format!("instance_metadata:{}", flow_task.flow_id))
-            .await
-        {
-            let json_str = String::from_utf8_lossy(&res);
-            let data: serde_json::Value = serde_json::from_str(&json_str)?;
-            let flow_type = data["flow_type"].as_str().unwrap();
-            let user_id = data["user_id"].as_str().unwrap();
-            if let Ok(res) = cl
-                .read_entry(&format!("flows:{}:default_dispatch_point", flow_type))
-                .await
-            {
-                dispatch_point = String::from_utf8_lossy(&res).to_string();
-            }
-            if let Ok(res) = cl
-                .read_entry(&format!(
-                    "flows:{}:mounts:{}:{}:dispatch_point_override",
-                    flow_type, user_id, flow_task.flow_id
-                ))
-                .await
-            {
-                dispatch_point = String::from_utf8_lossy(&res).to_string();
-            }
-        }
         let mut queues = QUEUES.lock().await;
         if !queues.contains_key(&flow_task.flow_id) {
+            let mut dispatch_point = "coflows_dispatch".to_string();
+            if let Ok(res) = cl
+                .read_entry(&format!("instance_metadata:{}", flow_task.flow_id))
+                .await
+            {
+                let json_str = String::from_utf8_lossy(&res);
+                let data: serde_json::Value = serde_json::from_str(&json_str)?;
+                let flow_type = data["flow_type"].as_str().unwrap();
+                let user_id = data["user_id"].as_str().unwrap();
+                if let Ok(res) = cl
+                    .read_entry(&format!("flows:{}:default_dispatch_point", flow_type))
+                    .await
+                {
+                    dispatch_point = String::from_utf8_lossy(&res).to_string();
+                }
+                if let Ok(res) = cl
+                    .read_entry(&format!(
+                        "flows:{}:mounts:{}:{}:dispatch_point_override",
+                        flow_type, user_id, flow_task.flow_id
+                    ))
+                    .await
+                {
+                    dispatch_point = String::from_utf8_lossy(&res).to_string();
+                }
+            }
             queues.insert(
                 flow_task.flow_id.clone(),
                 (dispatch_point.clone(), VecDeque::new()),
             );
         }
-        let (_, queue) = queues.get_mut(&flow_task.flow_id).unwrap();
+        let (dispatch_point, queue) = queues.get_mut(&flow_task.flow_id).unwrap();
+        let dispatch_point = dispatch_point.clone();
         if queue.is_empty() {
             queue.push_back(message_id.clone());
             drop(queues);
